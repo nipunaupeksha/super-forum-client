68cf56e3eb1ec323ea0c68cd560459b24ef28d08
nipunaupeksha <nipuna.upeksha@gmail.com>
2023-02-15 23:57:17 +0530

    files added

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import configureStore from "./store/configureStore";
+import ErrorBoundary from "./components/ErrorBoundary";
+import ReactModal from "react-modal";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql",
+  credentials: "include",
+  cache: new InMemoryCache({
+    resultCaching: false,
+  }),
+});
+
+ReactDOM.render(
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <ApolloProvider client={client}>
+        <ErrorBoundary>{[<App key="App" />]}</ErrorBoundary>
+      </ApolloProvider>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactModal.setAppElement("#root");
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
